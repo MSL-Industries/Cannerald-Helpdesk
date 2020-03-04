685c8e5ff39b9d33ca4dafee5be9200e97d1d543
Tune389 <sascha.waeschle@d4ho.de>
2020-03-04 10:16:18 +0100

    Fix error from chat message sender

--- app/Listeners/SendChatMessage.php
+++ app/Listeners/SendChatMessage.php
@@ -20,38 +20,34 @@ class SendChatMessage
     /**
      * Handle the event.
      *
-     * @param  TicketCreated  $event
+     * @param TicketCreated $event
      *
      * @return void
      */
     public function handle(TicketCommented $event)
     {
 
-        $client = new Client();
-
-        $options = [
-            'form_params' => [
-                'message' => $event->comment->body,
-                'api_key' => env('API_TOKEN')
-            ]
-        ];
-
-        $options['headers'] = [
-            'Accept' => 'application/json',
-            'Content-Type' => 'application/x-www-form-urlencoded'
-        ];
-
-        $res = $client->request('post', env('APP_AUTH_ENDPOINT') . 'support/ticket/' . $event->ticket->id . '/messages', $options);
-        return $this->decodeBody($res);
-    }
-
-    public function decodeBody($response)
-    {
         try {
-            return \GuzzleHttp\json_decode($response->getBody());
+            $client = new Client();
+
+            $options = [
+                'form_params' => [
+                    'message' => $event->comment->body,
+                    'api_key' => env('API_TOKEN')
+                ]
+            ];
+
+            $options['headers'] = [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/x-www-form-urlencoded'
+            ];
+
+            $client->request('post', env('APP_AUTH_ENDPOINT') . 'support/ticket/' . $event->ticket->id . '/messages', $options);
+
         } catch (\Exception $e) {
-            echo $response->getBody();
-            exit;
+
         }
+
     }
+
 }
