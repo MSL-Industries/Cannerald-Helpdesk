a0f027360af8b61fd643c0d149324142f064d089
s5w95 <sascha.waeschle@d4ho.de>
2022-03-26 16:02:02 +0100

    Detect mails with recovery keyword

--- app/Jobs/EmailParsers/NewTicketEmailParser.php
+++ app/Jobs/EmailParsers/NewTicketEmailParser.php
@@ -4,13 +4,19 @@ namespace App\Jobs\EmailParsers;
 
 use App\Attachment;
 use App\Ticket;
+use App\TicketType;
 
 class NewTicketEmailParser
 {
     public function handle($message)
     {
+        /** @var Ticket $ticket */
         $ticket = Ticket::createAndNotify(['name' => $message->fromName, 'email' => $message->fromAddress], $message->subject, $message->body(), ['email']);
         Attachment::storeAttachmentsFromEmail($message, $ticket);
+
+        if (strpos(strtolower($message->subject), 'recovery') !== false) {
+            $ticket->type()->associate(TicketType::where('name', 'recovery')->first());
+        }
 
         return true;
     }
